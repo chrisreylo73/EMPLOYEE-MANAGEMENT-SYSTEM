f787f6ba1f245cc8d7e9eef7bd7bd1df22472d45
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-29 17:03:55 -0600

    Removed useless stuff

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div className="flex justify-center items-center h-screen ">
+    <h1 className="text-3xl font-bold flex">
       Hello world!
-    </h1>
+      </h1>
+    </div>
   );
 }
 
---
+++ react-frontend/src/components/ListEmployeesComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListEmployeesComponent = () => {
+  return (
+    <div>
+      <h2>EMPLOYEES LIST</h2>
+    </div>
+  )
+}
+
+export default ListEmployeesComponent
